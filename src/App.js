3a0df45cd3d4b6d7f9a7841a203226f2894690c7
Francis Rodrigues <dev@tosystems.net>
2017-11-13 16:09:25 -0200

    Added bootstrap to the project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+
+class Header extends Component {
+  render() {
+    return <h1>MercadoDev</h1>
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
@@ -14,8 +20,8 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
