7a97d77fa9b3a9f2f518595ad751f168af0a47ba
Francis Rodrigues <dev@tosystems.net>
2017-11-13 20:12:57 -0200

    Added the first route and a test route

--- src/App.js
+++ src/App.js
@@ -4,15 +4,20 @@ import Footer from './Footer'
 import Home from './Home'
 
 import base from './base'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
+const Test = () => <h1>Test</h1>
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Home />
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <Route path='/' exact component={Home} />
+          <Route path='/test' exact component={Test} />
+          <Footer />
+        </div>
+      </Router>
     )
   }
 }
