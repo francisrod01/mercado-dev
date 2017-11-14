71862305ea8724cc3dc9ded20a32f8b41225bc02
Francis Rodrigues <dev@tosystems.net>
2017-11-14 00:56:06 -0200

    Setted firebase storage to the base config

--- src/base.js
+++ src/base.js
@@ -3,15 +3,18 @@ const config = {
     authDomain: "devpleno-first-app.firebaseapp.com",
     databaseURL: "https://devpleno-first-app.firebaseio.com",
     projectId: "devpleno-first-app",
-    storageBucket: "devpleno-first-app.appspot.com",
+    storageBucket: "gs://devpleno-first-app.appspot.com",
     messagingSenderId: "468785962232"
 }
 
 const Rebase = require('re-base')
 const firebase = require('firebase/app')
 require('firebase/database')
+require('firebase/storage')
 
 const app = firebase.initializeApp(config)
 const base = Rebase.createClass(app.database())
+
+export const storage = app.storage()
 
 export default base
