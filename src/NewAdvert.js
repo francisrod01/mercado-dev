954d5bc8c19aa7d69dfe60f02b20fe6902819cd9
Francis Rodrigues <dev@tosystems.net>
2017-11-13 23:32:54 -0200

    Implemented form to the NewAdvert component

--- src/NewAdvert.js
+++ src/NewAdvert.js
@@ -3,7 +3,17 @@ import React, {Component} from 'react'
 class NewAdvert extends Component {
     render() {
         return (
-            <h1>New Advert</h1>
+            <div>
+                <div className='container' style={{paddingTop: '120px'}}>
+                    <h1>New Advert</h1>
+                    <form onSubmit={null}>
+                        <div className='form-group'>
+                            <label htmlFor='name'>Name</label>
+                            <input type='text' className='form-control' id='name' placeholder='name' />
+                        </div>
+                    </form>
+                </div>
+            </div>
         )
     }
 }
