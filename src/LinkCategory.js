51c8b6f41e7f233b53655e0f33f1cf31805b9a7e
Francis Rodrigues <dev@tosystems.net>
2017-11-14 01:09:10 -0200

    FIX: Name of category in LinkCategory component

--- src/LinkCategory.js
+++ src/LinkCategory.js
@@ -4,7 +4,7 @@ const LinkCategory = ({category}) => {
     return (
         <a to={`/adverts/category/`} className="btn btn-secondary h-100 m-2 col-sm">
             <i className={`fa ${category.icon} fa-4x`} aria-hidden="true"></i><br />
-            {category.name}
+            {category.category}
         </a>
     )
 }
