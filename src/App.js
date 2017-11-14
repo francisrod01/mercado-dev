0349bee510f7858eb5e38d210d7cb9d48f41b431
Francis Rodrigues <dev@tosystems.net>
2017-11-13 23:05:27 -0200

    Remove Route test to App.js

--- src/App.js
+++ src/App.js
@@ -6,15 +6,12 @@ import Home from './Home'
 import base from './base'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 
-const Test = () => <h1>Test</h1>
-
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
           <Route path='/' exact component={Home} />
-          <Route path='/test' exact component={Test} />
           <Footer />
         </div>
       </Router>
